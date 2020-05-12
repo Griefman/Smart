76e3a26583b5a09bc2a765557b76e1b53d67d231
Griefman <stanislavparshin@yandex.ru>
2020-05-12 20:09:19 +0300

    Добвляет открытие\закрытие попапа

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,59 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var KEY_ESC_CODE = 27;
+var popUp = document.querySelector('.popup-form');
+var overlay = document.querySelector('.modal-layout');
+var buttonOpenPopUp = document.querySelector('.page-header__button');
+var buttonClosePopUp = document.querySelector('.popup-form__button-close');
 
-pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+/**
+ * Открывает  попап.
+ * @param {event} evt - Событие клика
+ */
+
+buttonOpenPopUp.addEventListener('click', function () {
+  if (popUp.classList.contains('display-hidden')) {
+    popUp.classList.remove('display-hidden');
+    overlay.classList.remove('display-hidden');
   }
 });
+
+
+/**
+ * Закрывает  попап.
+ */
+
+var closePopUp = function () {
+  if (!popUp.classList.contains('display-hidden')) {
+    popUp.classList.add('display-hidden');
+    overlay.classList.add('display-hidden');
+  }
+}
+
+buttonClosePopUp.addEventListener('click', function () {
+  closePopUp();
+});
+
+overlay.addEventListener('click', function () {
+  closePopUp();
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KEY_ESC_CODE) {
+    closePopUp();
+  }
+});
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,49 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var KEY_ESC_CODE = 27;
+var popUp = document.querySelector('.popup-form');
+var layuot = document.querySelector('.modal-layout');
+var buttonOpenPopUp = document.querySelector('.page-header__button');
+var buttonClosePopUp = document.querySelector('.popup-form__button-close');
 
-pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+/**
+ * Открывает  попап.
+ * @param {event} evt - Событие клика
+ */
+
+buttonOpenPopUp.addEventListener('click', function () {
+  if (popUp.classList.contains('display-hidden')) {
+    popUp.classList.remove('display-hidden');
+    layuot.classList.remove('display-hidden');
   }
 });
+
+
+/**
+ * Закрывает  попап.
+ */
+
+var closePopUp = function () {
+  if (!popUp.classList.contains('display-hidden')) {
+    popUp.classList.add('display-hidden');
+    layuot.classList.add('display-hidden');
+  }
+}
+
+buttonClosePopUp.addEventListener('click', function () {
+  closePopUp();
+});
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
