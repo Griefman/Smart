af2915c46b545f1a3b085e7cfa17e856f4008051
Griefman <stanislavparshin@yandex.ru>
2020-05-12 21:30:21 +0300

    Добавляет аккордеон

--- build/js/script.js
+++ build/js/script.js
@@ -4,6 +4,10 @@ var popUp = document.querySelector('.popup-form');
 var layuot = document.querySelector('.modal-layout');
 var buttonOpenPopUp = document.querySelector('.page-header__button');
 var buttonClosePopUp = document.querySelector('.popup-form__button-close');
+var toggleSite = document.querySelector('.footer-info__site-toggle');
+var siteColumns = document.querySelector('.footer-info__site-columns');
+var toggleOffice = document.querySelector('.footer-info__office-toggle');
+var officeColumns = document.querySelector('.footer-info__office-columns');
 
 
 /**
@@ -32,6 +36,36 @@ var closePopUp = function () {
 
 buttonClosePopUp.addEventListener('click', function () {
   closePopUp();
+});
+
+
+toggleSite.addEventListener('click', function () {
+  if (toggleSite.classList.contains('toggle-plus')) {
+    toggleSite.classList.remove('toggle-plus');
+    toggleSite.classList.add('toggle-minus');
+    siteColumns.style.display = 'block';
+  } else {
+    if (toggleSite.classList.contains('toggle-minus')) {
+      toggleSite.classList.remove('toggle-minus');
+      toggleSite.classList.add('toggle-plus');
+      siteColumns.style.display = 'none';
+    }
+  }
+});
+
+
+toggleOffice.addEventListener('click', function () {
+  if (toggleOffice.classList.contains('toggle-plus')) {
+    toggleOffice.classList.remove('toggle-plus');
+    toggleOffice.classList.add('toggle-minus');
+    officeColumns.style.display = 'block';
+  } else {
+    if (toggleOffice.classList.contains('toggle-minus')) {
+      toggleOffice.classList.remove('toggle-minus');
+      toggleOffice.classList.add('toggle-plus');
+      officeColumns.style.display = 'none';
+    }
+  }
 });
 // var pageHeader = document.querySelector('.page-header');
 // var headerToggle = document.querySelector('.page-header__toggle');
